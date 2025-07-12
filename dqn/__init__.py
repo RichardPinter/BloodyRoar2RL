4b1c79cd27a4ef5be9bf5f64c627c213b2ff7d8d
Richard <richardpinter92@gmail.com>
2025-07-12 16:50:48 +0930

    first commit atari

---
+++ dqn/__init__.py
@@ -0,0 +1,9 @@
+"""
+Deep Q-Network (DQN) Implementation for Vision-Based RL
+
+This package implements DQN with screenshot-based learning,
+optimized for CPU training at 1 FPS gameplay.
+"""
+
+__version__ = "1.0.0"
+__author__ = "DQN Implementation for Bloody Roar 2"
